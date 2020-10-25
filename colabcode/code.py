57de22cedd69c0af41e9483caeaa3a02e21ae84d
yongjie <yongjie@6estates.com>
2020-10-25 14:38:36 +0800

    addeded extension

--- colabcode/code.py
+++ colabcode/code.py
@@ -9,11 +9,12 @@ try:
 except ImportError:
     colab_env = False
 
-
 EXTENSIONS = ["ms-python.python", "jithurjacob.nbpreviewer"]
 import sys
+
+
 class Connector:
-    def __init__(self,port,option="localtunnel"):
+    def __init__(self, port, option="localtunnel"):
         self.port = port
         self.option = option
         self.connection = None
@@ -23,9 +24,9 @@ class Connector:
             url = ngrok.connect(port=self.port, options={"bind_tls": True})
             print(f"Code Server can be accessed on: {url}")
         elif self.option == "localtunnel":
-            self.connection = subprocess.Popen(["lt","--port",str(10000)],stdout=subprocess.PIPE)
-            
-            
+            self.connection = subprocess.Popen(["lt", "--port", str(10000)],
+                                               stdout=subprocess.PIPE)
+
     def disconnect(self):
         if self.option == "localtunnel":
             try:
@@ -37,26 +38,31 @@ class Connector:
             for tunnel in active_tunnels:
                 public_url = tunnel.public_url
                 ngrok.disconnect(public_url)
+
     def get_url(self):
         if self.option == "localtunnel":
-            for line in iter(self.connection.stdout.readline,''):
+            for line in iter(self.connection.stdout.readline, ''):
                 print (line.rstrip())
                 break
-        
+
 
 class ColabCode:
-    def __init__(self, port=10000, password=None, mount_drive=False,option="localtunnel", add_extensions=None):
+    def __init__(self, port=10000, password=None, mount_drive=False,
+                 option="localtunnel", add_extensions=None):
         self.port = port
-        self.connection = Connector(self.port,option)
+        self._install_localtunnel()
+        self.connection = Connector(self.port, option)
         self.password = password
         self._mount = mount_drive
         self._install_code()
         self.extensions = EXTENSIONS
         if add_extensions is not None and add_extensions != []:
-            if isinstance(add_extensions, list) and isinstance(add_extensions[0], str):
+            if isinstance(add_extensions, list) and isinstance(
+                    add_extensions[0], str):
                 self.extensions += add_extensions
             else:
-                raise TypeError("You need to pass a list of string(s) e.g. ['ms-python.python']")
+                raise TypeError(
+                    "You need to pass a list of string(s) e.g. ['ms-python.python']")
         self._install_extensions()
         self._start_server()
         self.connection.get_url()
@@ -64,13 +70,16 @@ class ColabCode:
 
     def _install_code(self):
         subprocess.run(
-            ["wget", "https://code-server.dev/install.sh"], stdout=subprocess.PIPE
+            ["wget", "https://code-server.dev/install.sh"],
+            stdout=subprocess.PIPE
         )
         subprocess.run(["sh", "install.sh"], stdout=subprocess.PIPE)
+
     def _install_localtunnel(self):
         subprocess.run(
-            ["npm", "install", "-g" ,"localtunnel"], stdout=subprocess.PIPE
+            ["npm", "install", "-g", "localtunnel"], stdout=subprocess.PIPE
         )
+
     def _install_extensions(self):
         for ext in EXTENSIONS:
             subprocess.run(["code-server", "--install-extension", f"{ext}"])
@@ -88,11 +97,11 @@ class ColabCode:
         else:
             code_cmd = f"code-server --port {self.port} --auth none --disable-telemetry"
         with subprocess.Popen(
-            [code_cmd],
-            shell=True,
-            stdout=subprocess.PIPE,
-            bufsize=1,
-            universal_newlines=True,
+                [code_cmd],
+                shell=True,
+                stdout=subprocess.PIPE,
+                bufsize=1,
+                universal_newlines=True,
         ) as proc:
             for line in proc.stdout:
                 print(line, end="")
