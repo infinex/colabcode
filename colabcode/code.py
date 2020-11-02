3bda115dc4d9d058b6625e97802cae9dbd3d79d4
yongjie <yongjie@6estates.com>
2020-11-02 13:55:51 +0800

    improve args for tunnel

--- colabcode/code.py
+++ colabcode/code.py
@@ -25,8 +25,10 @@ class Connector:
             url = ngrok.connect(port=self.port, options={"bind_tls": True})
             print(f"Code Server can be accessed on: {url}")
         elif self.option == "localtunnel":
+            cmd = "lt --port 10000 " + f"{self.args}",
+            print(cmd)
             self.connection = subprocess.Popen(
-                "lt --port 10000 " + f"{self.args}",
+                cmd ,
                 stdout=subprocess.PIPE,
                 shell=True)
 
